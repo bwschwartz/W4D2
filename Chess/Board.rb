135a339ff0304f12f4ae7ccc01a8719441629341
Benjamin Schwartz <bs4308@nyu.edu>
2022-08-09 14:01:36 -0400

    Fill board initial

--- Chess/Board.rb
+++ Chess/Board.rb
@@ -1,6 +1,39 @@
+require_relative "piece"
+
 class Board
+  def self.fill_board(board)
+    piece_rows = [0, 1, 6, 7]
+    board.map!.with_index do |row, i|
+      row.map! do |square|
+        square = Piece.new if piece_rows.include?(i)
+      end
+    end
+  end
+
+  attr_accessor :board
+
   def initialize
     @board = Array.new(8) { Array.new(8) }
+    Board.fill_board(@board)
+  end
+
+  def move_piece(start_pos, end_pos)
+    return false if empty?(start_pos) || !valid_pos?(end_pos)
+
+    piece = self[start_pos]
+    self[end_pos] = piece
+    self[start_pos] = nil
+    true
+  end
+
+  def empty?(pos)
+    return true if pos == nil
+    false
+  end
+
+  def valid_pos?(pos)
+    pos.each { |dim| return false if dim < 0 || dim > 7 }
+    true
   end
 
   def [](pos)
@@ -11,12 +44,6 @@ class Board
   def []=(pos, val)
     row, col = pos
     @board[row][col] = val
-  end
-
-  def move_piece(color, start_pos, end_pos)
-  end
-
-  def valid_pos?(pos)
   end
 
 end
