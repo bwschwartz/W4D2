6cdfb8a1354b4d54612d0d7d885d39d8c2d656e1
Benjamin Schwartz <bs4308@nyu.edu>
2022-08-09 12:17:15 -0400

    Created board basic functionality

---
+++ Chess/Board.rb
@@ -0,0 +1,22 @@
+class Board
+  def initialize
+    @board = Array.new(8) { Array.new(8) }
+  end
+
+  def [](pos)
+    row, col = pos
+    @board[row][col]
+  end
+
+  def []=(pos, val)
+    row, col = pos
+    @board[row][col] = val
+  end
+
+  def move_piece(color, start_pos, end_pos)
+  end
+
+  def valid_pos?(pos)
+  end
+
+end
